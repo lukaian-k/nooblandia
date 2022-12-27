a9b0f5cf7b24bba175f91a48d16722b3190d7b76
Lucas José <lukinhas8979@gmail.com>
2022-12-26 23:31:19 -0300

    primeiras alimentacoes de reply

--- src/reply/defaults.py
+++ src/reply/defaults.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from src.system.json import *
 from src.system.directories import *
 
@@ -6,9 +8,13 @@ async def defaults(message):
     DEFAULTS = dict(read_json(DIR_DEFAULTS))
     KEYS = list(DEFAULTS.keys())
 
-    for word in KEYS:
-        m = message.content.lower()
+    for key in KEYS:
+        for word in DEFAULTS[key]["var"]:
+            word = word.lower()
+            m = message.content.lower()
 
-        if m.startswith(word.lower()):
-            await message.channel.send(DEFAULTS[word])
-            print(f"{DEFAULTS[word]} | SEND |")
+            if word in m:
+                send = choice(DEFAULTS[key]["reply"])
+                await message.channel.send(send)
+
+                print(f"{send} | SEND |"); break
