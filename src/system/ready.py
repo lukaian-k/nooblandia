b52ef59620861c4e3177129dc74630ad8c7f2337
Lucas José <lukinhas8979@gmail.com>
2023-01-02 01:03:06 -0300

    modo streaming add ao dict activity

--- src/system/ready.py
+++ src/system/ready.py
@@ -3,13 +3,37 @@ import discord
 
 async def ready(client):
     activity = {
-        "game": discord.Game(name='Minecraft', type=3),
-        "listening": discord.Activity(type=discord.ActivityType.listening, name="Here Always (SEUNGMIN of Stray Kids)"),
+        "game": discord.Game(
+            name='Minecraft',
+            type=3
+        ),
+
+        "listening": discord.Activity(
+            type=discord.ActivityType.listening,
+            name="Here Always (SEUNGMIN of Stray Kids)"
+        ),
+
+        "streaming": discord.Streaming(
+            name='Minecraft com Mod!',
+            game='Minecraft',
+
+            platform='Twitch',
+            twitch_name='rahnoob',
+            url='https://www.twitch.tv/rahnoob',
+
+            assets={
+                "large_image": 'assets/img/large_image',
+                "large_text": 'Olá! Venha conhecer meu canal na Twitch!',
+
+                "small_image": 'assets/img/small_image',
+                "small_text": 'Oii! Como que cê ta?!',
+            }
+        ),
     }
 
     await client.change_presence(
         status=discord.Status.online,
-        activity=activity["listening"]
+        activity=activity["streaming"]
     )
 
     print(f'Init: {client.user.name}')
