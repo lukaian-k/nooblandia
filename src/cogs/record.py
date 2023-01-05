23732e999da5e89d0727214989928b615784b178
Lucas José <lukinhas8979@gmail.com>
2023-01-05 13:58:35 -0300

    melhorando o record; agr pode iniciar o audio com o emoji de play

--- src/cogs/record.py
+++ src/cogs/record.py
@@ -5,24 +5,54 @@ import pyaudio
 import wave
 
 
-class Tests(commands.Cog):
+class Record(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+        self.ctx = None
+        self.duration = None
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        bot = self.bot
+        
+        message = reaction.message
+        emoji = str(reaction.emoji)
+
+        if user != bot.user and emoji == '▶':
+            await message.remove_reaction('▶', user)
+            
+            await message.remove_reaction('▶', bot.user)
+            await message.remove_reaction('❌', bot.user)
+            
+            await self.recording(message)
+
+        if user != bot.user and emoji == '❌':
+            embed = discord.Embed(
+                colour=15548997,
+                title='Cancelado!'
+            )
+            await self.ctx.send(
+                embed=embed,
+                delete_after=10
+            )
+            await message.delete()
+
 
     @commands.command(
         name='record',
         help='Grave audios para enviar no discord!',
-        aliases=['r', 'gravar'],
+        aliases=['r','gravar'],
     )
     @commands.has_permissions(
         administrator=True,
     )
     async def record(self, ctx, duration:int=10):
         embed = discord.Embed()
-
+        
         if not ctx.author.voice:
             embed.colour = 15548997
-            embed.title = "Você precisa estar em um canal de voz para usar esse comando."
+            embed.title = 'Você precisa estar em um canal de voz para usar esse comando.'
             await ctx.reply(
                 embed=embed,
                 delete_after=10
@@ -38,7 +68,29 @@ class Tests(commands.Cog):
                 delete_after=10
             )
             return
-            
+
+        self.ctx = ctx
+        self.duration = duration
+
+
+        embed.colour=5793266
+        embed.title='Gravador de Audios'
+        embed.description=f'Grave e envie audios no **Discord**'
+
+        embed.add_field(
+            name='Como usar:',
+            value=f'Pressione o botão ▶ para gravar o audio.\n\nDuração do audio: **{duration}s**',
+            inline=False
+        )
+        message = await ctx.send(embed=embed)
+
+        await message.add_reaction('▶')
+        await message.add_reaction('❌')
+
+
+    async def recording(self, message):
+        ctx = self.ctx
+        duration = self.duration
 
         author = ctx.message.author
         channel = author.voice.channel
@@ -63,10 +115,13 @@ class Tests(commands.Cog):
         )
         audio_data = []
 
-        embed.colour = 16705372
-        embed.title = '➭ Gravação iniciada!'
-        embed.description = f'Duração da gravação: **{duration}s**'
-        message = await ctx.reply(embed=embed)
+        embed = discord.Embed(
+            colour = 16705372,
+
+            title = '➭ Gravação iniciada!',
+            description = f'Duração da gravação: **{duration}s**'
+        )
+        await message.edit(embed=embed)
 
         for i in range(0, int(RATE/CHUNK*SECONDS)):
             data = stream.read(CHUNK)
@@ -106,4 +161,4 @@ class Tests(commands.Cog):
 
 
 async def setup(bot):
-    await bot.add_cog(Tests(bot))
+    await bot.add_cog(Record(bot))
